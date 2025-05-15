4ad5528bf42c682bf783bd10f569ad0fbcffe458
Manas Das <dasmanas2308@gmail.com>
2025-05-15 12:23:48 +0530

    feat: implement flash screen visibility logic in dashboard component

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { FlashScreenComponent } from "./flash-screen/flash-screen.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [FlashScreenComponent],
+  imports: [FlashScreenComponent,CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+ showFlash = true;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.showFlash = false;
+    }, 10000); // 10 seconds
+  }
+
 
 }
