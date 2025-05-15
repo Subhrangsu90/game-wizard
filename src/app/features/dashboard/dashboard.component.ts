a57156fd98342abc1ec2e9b87a22284dff0e5412
Manas Das <dasmanas2308@gmail.com>
2025-05-15 12:45:05 +0530

    ui fix

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { FlashScreenComponent } from "./flash-screen/flash-screen.component";
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [FlashScreenComponent,CommonModule],
+  imports: [FlashScreenComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
@@ -15,7 +14,7 @@ export class DashboardComponent {
   ngOnInit() {
     setTimeout(() => {
       this.showFlash = false;
-    }, 10000); // 10 seconds
+    }, 5000); // 10 seconds
   }
 
 
